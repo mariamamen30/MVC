bb7d251eb8c83891993d6c520c82ea6c132d458b
mariam <mariam.amen30@gmail.com>
2020-05-05 22:25:35 +0200

    lab 1

---
+++ Lab 1/Lab 1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Lab_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registration(User user)
+        {
+            ViewBag.Name = user.Name;
+            ViewBag.Email = user.Email;
+            if(ViewBag.Name != null && ViewBag.Email != null)
+            {
+                return View("Welcome");
+            }
+            else return View("Registration");
+        }
+    }
+}
